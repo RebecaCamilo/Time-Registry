234211757267cef7c4df9444a16c5ad9d2c327eb
RebecaCamilo <rebecacamilo3@gmail.com>
2023-06-29 21:54:40 -0300

    feat: add methods in UserPort

--- src/main/java/com/project/timeRegistry/service/impl/UserService.java
+++ src/main/java/com/project/timeRegistry/service/impl/UserService.java
@@ -1,11 +1,13 @@
 package com.project.timeRegistry.service.impl;
 
 import com.project.timeRegistry.model.domain.User;
+import com.project.timeRegistry.model.request.UserRequest;
 import com.project.timeRegistry.repository.UserRepository;
 import com.project.timeRegistry.service.port.UserPort;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.hibernate.ObjectNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +16,32 @@ public class UserService implements UserPort {
 
     private UserRepository userRepository;
 
+    @Override
+    public Page<User> getAll(Pageable pageable) {
+        return null;
+    }
+
     public User getById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException(id, "user"));
+    }
+
+    @Override
+    public User create(UserRequest userRequest) {
+        return null;
+    }
+
+    @Override
+    public User update(Long id, UserRequest userRequest) {
+        return null;
+    }
+
+    @Override
+    public User deactivate(Long id) {
+        return null;
+    }
+
+    @Override
+    public User activate(Long id) {
+        return null;
     }
 }
--- src/main/java/com/project/timeRegistry/service/port/UserPort.java
+++ src/main/java/com/project/timeRegistry/service/port/UserPort.java
@@ -1,20 +1,31 @@
 package com.project.timeRegistry.service.port;
 
 import com.project.timeRegistry.model.domain.User;
+import com.project.timeRegistry.model.request.UserRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 /**
  * <p>
  *     Contract that API must implements to query, create, update or delete a user.
  *     <ol>
- *         <li>getAll() - method to get a list of users.</li>
+ *         <li>getAll() - method to get a page of users.</li>
  *         <li>getById() - method to get a user by id.</li>
  *         <li>create() - method to create a user.</li>
  *         <li>update() - method to update a user registry.</li>
- *         <li>delete() - method to delete a user registry.</li>
+ *         <li>deactivate() - method to deactivate a user registry.</li>
+ *         <li>activate() - method to activate a user registry.</li>
  *     </ol>
  * </p>
  */
 public interface UserPort {
+
+    /**
+     * Get a Page of Users by a pageable
+     * @param pageable    Input parameters of a paging request.
+     * @return {@link Page} of {@link User}
+     */
+    Page<User> getAll(Pageable pageable);
 
     /**
      * Get a User by its identifier
@@ -22,4 +33,34 @@ public interface UserPort {
      * @return {@link User}
      */
     User getById(Long id);
+
+    /**
+     * Registry a new User from a UserRequest
+     * @param userRequest    The data to registry a User.
+     * @return {@link User}
+     */
+    User create(UserRequest userRequest);
+
+    /**
+     * Update a User registry by its identifier and a UserRequest
+     * @param id            Identifier of User.
+     * @param userRequest   The data to update a User.
+     * @return {@link User}
+     */
+    User update(Long id, UserRequest userRequest);
+
+    /**
+     * Set the of User to inactivate by its identifier
+     * @param id    Identifier of User.
+     * @return {@link User}
+     */
+    User deactivate(Long id);
+
+    /**
+     * Set the of User to activate by its identifier
+     * @param id    Identifier of User.
+     * @return {@link User}
+     */
+    User activate(Long id);
+
 }
