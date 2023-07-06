642e54f00b6a108451fbcb4ac684a48626d8b4fa
RebecaCamilo <rebecacamilo3@gmail.com>
2023-07-05 22:38:25 -0300

    feat: add User controller

--- src/main/java/com/project/timeRegistry/controller/UserController.java
+++ src/main/java/com/project/timeRegistry/controller/UserController.java
@@ -4,18 +4,43 @@ import com.project.timeRegistry.model.domain.User;
 import com.project.timeRegistry.service.impl.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController(value = "/users")
+import static com.project.timeRegistry.Path.*;
+
+@RestController(value = PATH_USER)
 @RequiredArgsConstructor
 public class UserController {
 
     private UserService userService;
 
-    @GetMapping("/{id}")
+    @GetMapping
+    public ResponseEntity<User> getAll(@PathVariable Long id) {
+        return null;
+    }
+
+    @GetMapping(ID)
     public ResponseEntity<User> getById(@PathVariable Long id) {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@PathVariable Long id) {
+        return null;
+    }
+
+    @PutMapping(ID)
+    public ResponseEntity<User> update(@PathVariable Long id) {
+        return null;
+    }
+
+    @DeleteMapping(ID + STATUS)
+    public ResponseEntity<User> deactivate(@PathVariable Long id) {
+        return null;
+    }
+
+    @PostMapping(ID + STATUS)
+    public ResponseEntity<User> activate(@PathVariable Long id) {
         return null;
     }
 }
