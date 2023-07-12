010074caa2b0c10001e5b4734d525f611b6d3599
RebecaCamilo <rebecacamilo3@gmail.com>
2023-07-12 00:30:15 -0300

    feat: add ObjectNotFoundException and DataIntegratyViolationException

---
+++ src/main/java/com/project/timeRegistry/exception/DataIntegratyViolationException.java
@@ -0,0 +1,9 @@
+package com.project.timeRegistry.exception;
+
+public class DataIntegratyViolationException extends RuntimeException {
+	
+	public DataIntegratyViolationException(String message) {
+		super(message);
+	}
+
+}
